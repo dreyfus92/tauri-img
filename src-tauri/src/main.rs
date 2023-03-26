20922da0a12b80f4e9c6a8ba4e7ee4bf182bbd71
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-03-25 18:15:39 -0600

    chore: refactored main.rs and changed styles of App.tsx

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,25 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs::File;
-use std::io::prelude::*;
-
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-#[tauri::command]
-fn read_file(path: &str) -> String {
-    let mut file = File::open(path).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    contents
+fn read_file(name: &str) -> String {
+    format!("The image you chose is {}!", name)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -7,40 +7,46 @@ import { DndContext } from '@dnd-kit/core';
 import { Droppable } from './components/Droppable';
 
 function App() {
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  //   setGreetMsg(await invoke('greet', { name }));
-  // }
-
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [imageInfo, setImageInfo] = useState<string | null>(null);
+
+  const { name } = selectedImage ?? { name: '' };
+
+  const handleConvert = async () => {
+    setImageInfo(await invoke('read_file', { name }));
+  };
 
   return (
     <Suspense fallback={<Loader />}>
-      <div className="flex flex-col items-center my-10 gap-y-5">
-        <div className="flex items-center gap-x-4 w-9/12 mb-10">
-          <h1>Welcome to Tauri Image Converter</h1>
-          <img src="./tauri.svg" className="w-20 h-20" alt="Tauri Logo" />
-        </div>
-        <DndContext>
-          <Droppable />
-        </DndContext>
-        <p>
-          You can drag in your image to convert it or click the button to upload
-          it.
-        </p>
-        <div className="flex items-center gap-x-4">
-          <Button>
-            <input
-              type="file"
-              name="myImage"
-              onChange={(e) => {
-                if (!e.target.files) return;
-                console.log(e.target.files[0]);
-                setSelectedImage(e.target.files[0]);
-              }}
-            />
-          </Button>
-          <Button>Convert</Button>
+      <div className="mx-auto my-10 w-9/12">
+        <div className="flex flex-col items-center gap-y-10">
+          <div className="flex items-center gap-x-6 mb-10">
+            <h1>Welcome to Tauri Image Converter</h1>
+            <img src="./tauri.svg" className="w-20 h-20" alt="Tauri Logo" />
+          </div>
+          <DndContext>
+            <Droppable />
+          </DndContext>
+          <p>
+            You can drag in your image to convert it or click the button to
+            upload it.
+          </p>
+          <div className="flex items-center gap-x-4">
+            <Button>
+              <input
+                type="file"
+                name="myImage"
+                onChange={(e) => {
+                  if (!e.target.files) return;
+                  console.log(e.target.files[0]);
+                  console.log(e.target.files[0].name);
+                  setSelectedImage(e.target.files[0]);
+                }}
+              />
+            </Button>
+            <Button onClick={handleConvert}>Convert</Button>
+          </div>
+          {imageInfo && <p>{imageInfo}</p>}
         </div>
       </div>
     </Suspense>
